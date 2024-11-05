8fee79c96395e0efcae97666e9dcba62e101764b
cappeLindo <vitorcappe@gmail.com>
2024-11-05 17:50:08 -0300

    alteração do header geral, ele já está completo

---
+++ TelasHtmleCSS/Vitor/header/script.js
@@ -0,0 +1,18 @@
+let menuAberto = false;
+
+function toggleMenu() {
+    const menu = document.getElementById("menuLateral");
+    const voltar = document.querySelector(".voltar");  
+
+    if (menuAberto) {
+        // Fecha o menu
+        menu.style.left = "-300px";  
+        voltar.style.left = "10px";  
+        menuAberto = false;
+    } else {
+        // Abre o menu
+        menu.style.left = "10px";  
+        voltar.style.left = "-300px";  
+        menuAberto = true;
+    }
+}
