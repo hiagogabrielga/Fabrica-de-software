e22528e4fa7220fe7cbc3191e7db5876f501e8f2
NathanTLopes <texeiranathan20@gmail.com>
2024-11-08 16:22:06 -0400

    tela de suporte pronta

--- TelasHtmleCSS/Nathan/TelaDeSuporte/script.js
+++ TelasHtmleCSS/Nathan/TelaDeSuporte/script.js
@@ -1,5 +1,33 @@
-document.getElementById('confirmButton').addEventListener('click', function() {
-    // Substitui o botão por um elemento não clicável
-    this.outerHTML = '<div class="non-clickable">Entraremos em contato</div>';
-  });
-  
+
+const supportButtons = document.querySelectorAll('.support-btn:not(.confirm-btn)');
+const confirmButton = document.getElementById('confirmButton');
+let isConfirmed = false; 
+
+
+supportButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!isConfirmed) { 
+            if (button.classList.contains('active')) {
+                button.classList.remove('active');
+            } else {
+                button.classList.add('active');
+            }
+        }
+    });
+});
+
+
+confirmButton.addEventListener('click', () => {
+    isConfirmed = true; 
+    supportButtons.forEach(button => {
+        button.disabled = true;
+        button.classList.add('locked'); 
+    });
+
+
+    const confirmationBlock = document.createElement('div');
+    confirmationBlock.classList.add('non-clickable');
+    confirmationBlock.textContent = 'Confirmação enviada';
+
+    confirmButton.replaceWith(confirmationBlock);
+});
