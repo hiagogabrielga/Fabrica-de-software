b0be81e1bcfb5f71f9929504b0c0130827e27b60
cappeLindo <vitorcappe@gmail.com>
2024-11-13 07:21:36 -0400

    ajustes

--- TelasHtmleCSS/Vitor/tela_inicial/script.js
+++ TelasHtmleCSS/Vitor/tela_inicial/script.js
@@ -1,19 +1,15 @@
 let menuAberto = false;
 
 function toggleMenu() {
-    const menu = document.getElementById("menuLateral");
-    const voltar = document.querySelector(".voltar");  
+    const menu = document.getElementById("menuLateral");  
 
     if (menuAberto) {
         // Fecha o menu
         menu.style.left = "-300px";  
-        voltar.style.left = "1px";  
-    
         menuAberto = false;
     } else {
         // Abre o menu
         menu.style.left = "10px";  
-        voltar.style.left = "-300px";  
         menuAberto = true;
     }
 }
