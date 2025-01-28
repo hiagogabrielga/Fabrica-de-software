474823c8c9872e425ee2ce4fd04f0937da5b9a92
cappeLindo <vitorcappe@gmail.com>
2025-01-28 08:51:09 -0400

    criação das partes principais

---
+++ webcars/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./footer.module.css"
+
+export default function Footer() { 
+    return(
+      <footer>
+            <div>
+                
+            </div>
+      </footer>
+    )
+}
