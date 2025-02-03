8a6eda0ec3d97097ead27b5c8874beb0bcfd7842
NathanTLopes <texeiranathan20@gmail.com>
2025-02-03 16:02:52 -0400

    footer feito e testado

--- webcars/src/components/Footer/index.js
+++ webcars/src/components/Footer/index.js
@@ -1,12 +1,58 @@
+'use client';
 import React from "react";
-import style from "./footer.module.css"
+import styles from "./Footer.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Footer() { 
-    return(
-      <footer>
-            <div>
-                
-            </div>
-      </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <div className={styles.logo}>
+          <img src="/images/logo.png" alt="Logo" />
+        </div>
+        <div className={styles.footerLinks}>
+          <div className={styles.column}>
+            <h3>Institucional</h3>
+            <ul>
+              <li><Link className={styles.link} href="#">Acessibilidade</Link></li>
+              <li><Link className={styles.link} href="#">Gerenciamento de cookies</Link></li>
+              <li><Link className={styles.link} href="#">Termos e condições</Link></li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h3>Comprar</h3>
+            <ul>
+              <li><Link className={styles.link} href="/Vitor/tela_filtros/filtros">Veículos novos</Link></li>
+              <li><Link className={styles.link} href="/Vitor/tela_filtros/filtros">Veículos semi-novos</Link></li>
+              <li><Link className={styles.link} href="/Vitor/tela_filtros/filtros">Veículos usados</Link></li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h3>Novidades</h3>
+            <ul>
+              <li><Link className={styles.link} href="/Vitor/tela_inicial/index">Os mais populares</Link></li>
+              <li><Link className={styles.link} href="/Vitor/tela_inicial/index">Promoções</Link></li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h3>Ajuda</h3>
+            <ul>
+              <li><Link className={styles.link} href="/Maria/cadastroConss/cadastroPess/cadastroPess">Criar conta</Link></li>
+              <li><Link className={styles.link} href="/Julia/index">Logar conta</Link></li>
+              <li><Link className={styles.link} href="/Carlos/perfil">Meu perfil</Link></li>
+              <li><Link className={styles.link} href="/Nathan/TelaDeSuporte/teladesuporte">Suporte</Link></li>
+              <li><Link className={styles.link} href="/Hiago/codigoFinal/telaAdicionarProduto">Vender veículo</Link></li>
+              <li><Link className={styles.link} href="#">Contato: +55 (69) 012345678</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footerBottom}>
+        <p>© 2024–2025 Webcars do Brasil Ltda. Todos os direitos do site reservado</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
