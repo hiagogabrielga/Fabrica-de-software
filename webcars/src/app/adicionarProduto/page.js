7245c9cdb9e24312ec3110d54a1ac327e7890507
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-02-11 10:04:28 -0400

    atualizar

---
+++ webcars/src/app/adicionarProduto/page.js
@@ -0,0 +1,11 @@
+import styles from './adicionarProduto.module.css'
+import { ArrowLeft } from "lucide-react"
+import React, {useState, useEffectb} from "react"
+
+export default function AdicionarCarro() {
+  return(
+    <div>
+      tela Adicionar carro
+    </div>
+  )
+}
