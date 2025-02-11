3b579d3536858343be9a8fc098f8096e54bc648f
cappeLindo <vitorcappe@gmail.com>
2025-02-11 10:03:21 -0400

    criação e estilização do Header, juntamente com pages

--- webcars/src/components/Header/index.js
+++ webcars/src/components/Header/index.js
@@ -1,11 +1,39 @@
-import React from "react";
-import styles from "./header.module.css"
+'use client'
 
+import React, { useState } from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { AlignJustify } from "lucide-react";
 export default function Header() {
-    return(
+    const [showMenulateral, setShowMenulateral] = useState(false)
+    return (
         <header>
-            <div>
-                
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
+            <div className={styles.cabecalho}>
+                <div className={styles.logo}>
+                    <Image src="/images/logo.png" alt="logo header" width={50} height={50} />
+                </div>
+                <div>
+                    <AlignJustify className={styles.tres} size={33} onClick={() => setShowMenulateral(!showMenulateral)} />
+                </div>
+                {showMenulateral &&
+                    <div className={styles.drop_menu}>
+                        <ul className={styles.drop_lu}>
+                            <li className={styles.drop_li}>
+                                <ul className={styles.sub_menu}>
+                                    <li className={styles.sub_menu_li}>
+                                        <Link href="#">Olá</Link>
+                                        <Link href="#">Olá</Link>
+                                        <Link href="#">Olá</Link>
+                                        <Link href="#">Olá</Link>
+                                        <Link href="#">Olá</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                }
             </div>
         </header>
     )
