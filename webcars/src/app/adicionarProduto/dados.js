eb9db9c9b58f8062c18929fea759a208a4d0a93b
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-02-11 13:23:06 -0400

    atualizar

---
+++ webcars/src/app/adicionarProduto/dados.js
@@ -0,0 +1,108 @@
+const cor = [
+    {
+      id_cor: 1,
+      nome_cor: "Vermelho"
+    },
+    {
+      id_cor: 2,
+      nome_cor: "Azul"
+    },
+    {
+      id_cor: 3,
+      nome_cor: "Preto"
+    }
+  ]
+
+const modelo = [
+    {
+      id_modelo: 1,
+      nome_modelo: "Corolla",
+      marca_id_marca: 1,
+      categoria_id_categoria: 2
+    },
+    {
+      id_modelo: 2,
+      nome_modelo: "Civic",
+      marca_id_marca: 2,
+      categoria_id_categoria: 2
+    },
+    {
+      id_modelo: 3,
+      nome_modelo: "Focus",
+      marca_id_marca: 3,
+      categoria_id_categoria: 3
+    }
+  ]
+
+const aro = [
+    {
+      id_aro: 1,
+      nome_aro: "Aro 15"
+    },
+    {
+      id_aro: 2,
+      nome_aro: "Aro 16"
+    },
+    {
+      id_aro: 3,
+      nome_aro: "Aro 17"
+    }
+  ]
+
+const combustivel = [
+    {
+      id_combustivel: 1,
+      nome_combustivel: "Gasolina"
+    },
+    {
+      id_combustivel: 2,
+      nome_combustivel: "Álcool"
+    },
+    {
+      id_combustivel: 3,
+      nome_combustivel: "Diesel"
+    }
+  ]
+
+const marca = [
+    {
+      id_marca: 1,
+      nome_marca: "Toyota"
+    },
+    {
+      id_marca: 2,
+      nome_marca: "Honda"
+    },
+    {
+      id_marca: 3,
+      nome_marca: "Ford"
+    }
+  ]
+
+const cambio = [
+    {
+      id_cambio: 1,
+      nome_cambio: "Manual"
+    },
+    {
+      id_cambio: 2,
+      nome_cambio: "Automático"
+    }
+  ]
+
+const categoria = [
+    {
+      id_categoria: 1,
+      nome_categoria: "SUV"
+    },
+    {
+      id_categoria: 2,
+      nome_categoria: "Sedan"
+    },
+    {
+      id_categoria: 3,
+      nome_categoria: "Hatch"
+    }
+  ]
+
+export {categoria, cambio, modelo, cor, marca, combustivel, aro }
