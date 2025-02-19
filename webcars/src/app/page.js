68de0f0fc39b7a9495303fb674d7bb5e09c35546
cappeLindo <vitorcappe@gmail.com>
2025-02-19 17:39:00 -0300

    craicao da page Main e os elementos dentro dela

--- webcars/src/app/page.js
+++ webcars/src/app/page.js
@@ -1,9 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <div className={styles.main}>
-      <p>aaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    <div>
+      <nav className={styles.nav}>
+        <ul className={styles.menu}>
+          <li className={styles.menu_item}>
+            <Link href="#">Categorias</Link>
+            <ul className={styles.dropdown}>
+              {[1, 2, 3, 4, 5].map((num) => (
+                <li key={num}><Link href="#">Categoria {num}</Link></li>
+              ))}
+            </ul>
+          </li>
+          <li className={styles.menu_item}><Link href="#">Catálogo 0Km</Link></li>
+          <li className={styles.menu_item}><Link href="#">Seminovos</Link></li>
+          <li className={styles.menu_item}><Link href="#">Vender</Link></li>
+        </ul>
+      </nav>
+
+      <div className={styles.destaque}>
+        <h1>Concessionárias em destaque</h1>
+        <div className={styles.fundo_cards}>
+          {["Concessionária 1", "Concessionária 2", "Concessionária 3", "Concessionária 4", "Concessionária 5"].map((item, index) => (
+            <div className={styles.cards_cs} key={index}>
+              <Image src="/images/fundo.jpg" alt="fundo" width={100} height={100}/>
+              <p>{item}</p>
+              <button><Link href="../../Nathan/TelaDaConcessionaria/teladaconcessionaria.html">veja mais</Link></button>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.carros_em_destaque}>
+        <h1>Carros em destaque</h1>
+        <div className={styles.fundo_carros}>
+          {new Array(6).fill("Volkswagen Gol").map((carro, index) => (
+            <div className={styles.card_carros} key={index}>
+              <Image src="/images/VW-Gol-lateral.jpg" alt="carro" width={160} height={120}/>
+              <p>{carro}</p>
+              <button><Link href="../../Carlos/descriçao-produto.html">veja mais</Link></button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
